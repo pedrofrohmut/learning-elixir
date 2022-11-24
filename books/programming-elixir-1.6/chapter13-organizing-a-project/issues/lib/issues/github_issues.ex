42cf5fe7cbe75c7715b42d59a6472ac0ea5faaa0
Pedro Frohmut <pedrofrohmut@gmail.com>
2022-11-24 18:18:26 -0300

    Chapter13 Part 5

--- books/programming-elixir-1.6/chapter13-organizing-a-project/issues/lib/issues/github_issues.ex
+++ books/programming-elixir-1.6/chapter13-organizing-a-project/issues/lib/issues/github_issues.ex
@@ -1,8 +1,11 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @user_agent [ { "User-agent", "Elixir dave@pragprog.com" } ]
   @github_url Application.compile_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info "Fetching #{user}'s project #{project}"
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response()
@@ -15,6 +18,8 @@ defmodule Issues.GithubIssues do
   def handle_response({ _, %{ status_code: status_code, body: body } }) do
     res_code = if(status_code == 200, do: :ok, else: :error)
     res_body = Poison.Parser.parse!(body)
+    Logger.info("Got response with status code of #{status_code}")
+    Logger.debug(fn -> inspect(res_body) end)
     { res_code, res_body }
   end
 end
