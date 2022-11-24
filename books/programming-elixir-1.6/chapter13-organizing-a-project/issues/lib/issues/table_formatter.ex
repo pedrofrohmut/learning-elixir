5c14b18343b2a8688ada854863bbe0c4c327c043
Pedro Frohmut <pedrofrohmut@gmail.com>
2022-11-23 23:26:52 -0300

    Chapter13 Part 3

--- books/programming-elixir-1.6/chapter13-organizing-a-project/issues/lib/issues/table_formatter.ex
+++ books/programming-elixir-1.6/chapter13-organizing-a-project/issues/lib/issues/table_formatter.ex
@@ -1,17 +1,53 @@
 defmodule Issues.TableFormatter do
-  def split_into_columns(rows, headers) do
+  def split_into_columns(rows, headers), do: (
       for header <- headers, do: (
           for row <- rows, do: (
               if(is_binary(row[header]), do: row[header], else: to_string(row[header]))
           )
       )
-  end
+  )
 
-  def widths_of(columns) do
+  def widths_of(columns), do: (
       for cells <- columns, do: (
           Enum.reduce(cells, 0, fn cell, acc ->
               if(String.length(cell) > acc, do: String.length(cell), else: acc)
           end)
       )
-  end
+  )
+
+  def format_for(column_widths), do: (
+      column_widths
+      |> Enum.map(fn x -> "~-#{x}s" end)
+      |> Enum.join(" | ")
+      |> Kernel.<>("~n")
+  )
+
+  def format_and_print_table(data, headers), do: (
+      data_by_columns = split_into_columns(data, headers)
+      column_widths = widths_of(data_by_columns)
+      format = format_for(column_widths)
+      print_table(headers, data_by_columns, column_widths, format)
+  )
+
+  def print_table(headers, data_by_columns, column_widths, format), do: (
+      print_table_header(headers, format)
+      print_table_separator(column_widths)
+      print_table_body(data_by_columns, format)
+  )
+
+  def print_table_header(fields, format), do: :io.format(format, fields)
+
+  def print_table_separator(column_widths), do: (
+      column_widths
+      |> Enum.map(fn width -> List.duplicate("-", width) end)
+      |> Enum.join("-+-")
+      |> IO.puts
+  )
+
+  def print_table_body(data, format), do: (
+      data
+      |> List.zip
+      |> Enum.map(fn x -> Tuple.to_list(x) end)
+      |> Enum.each(fn x -> :io.format(format, x) end)
+  )
 end
