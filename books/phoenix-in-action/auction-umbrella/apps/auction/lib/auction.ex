a2883ca9d3976b407834215576a7b94c406505d1
Pedro Frohmut <pedrofrohmut@gmail.com>
2022-12-08 21:50:24 -0300

    Chapter9 - Web Pages

--- books/phoenix-in-action/auction-umbrella/apps/auction/lib/auction.ex
+++ books/phoenix-in-action/auction-umbrella/apps/auction/lib/auction.ex
@@ -9,11 +9,15 @@ defmodule Auction do
 
     def get_item_by(attrs), do: @repo.get_by(Item, attrs)
 
-    def insert_item(attrs), do: Item |> struct(attrs) |> @repo.insert()
+    def new_item(), do: Item.changeset(%Item{})
+
+    def insert_item(attrs), do: %Item{} |> Item.changeset(attrs) |> @repo.insert()
 
     # Use '%Auction.Item', using pattern matching, to make sure you are passing and item
     # and don't delete a row from another table by mistake
     def delete_item(%Auction.Item{} = item), do: @repo.delete(item)
+
+    def edit_item(id), do: @repo.get!(Item, id) |> Item.changeset()
 
     def update_item(%Auction.Item{} = item, updates) do
         item |> Item.changeset(updates) |> @repo.update()
--- books/phoenix-in-action/auction-umbrella/apps/auction_web/lib/auction_web/router.ex
+++ books/phoenix-in-action/auction-umbrella/apps/auction_web/lib/auction_web/router.ex
@@ -1,56 +1,66 @@
 defmodule AuctionWeb.Router do
-  use AuctionWeb, :router
+    use AuctionWeb, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, {AuctionWeb.LayoutView, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
-  scope "/", AuctionWeb do
-    pipe_through :browser
-
-    get "/", PageController, :index
-  end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", AuctionWeb do
-  #   pipe_through :api
-  # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-
-      live_dashboard "/dashboard", metrics: AuctionWeb.Telemetry
+    pipeline :browser do
+        plug :accepts, ["html"]
+        plug :fetch_session
+        plug :fetch_live_flash
+        plug :put_root_layout, {AuctionWeb.LayoutView, :root}
+        plug :protect_from_forgery
+        plug :put_secure_browser_headers
     end
-  end
 
-  # Enables the Swoosh mailbox preview in development.
-  #
-  # Note that preview only shows emails that were sent by the same
-  # node running the Phoenix server.
-  if Mix.env() == :dev do
-    scope "/dev" do
-      pipe_through :browser
-
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    pipeline :api do
+        plug :accepts, ["json"]
     end
-  end
+
+    scope "/", AuctionWeb do
+        pipe_through :browser
+
+        get "/", PageController, :index
+
+        # These routes are the default ones that are created with resources
+        # get    "/items",          ItemsController, :index
+        # get    "/items/new",      ItemsController, :new
+        # post   "/items",          ItemsController, :create
+        # get    "/items/:id",      ItemsController, :show
+        # get    "/items/:id/edit", ItemsController, :edit
+        # patch  "/items/:id",      ItemsController, :update
+        # put    "/items/:id",      ItemsController, :update
+        # delete "/items/:id",      ItemsController, :delete
+        resources "/items", ItemsController, only: [:index, :show, :new, :create, :edit, :update]
+    end
+
+    # Other scopes may use custom stacks.
+    # scope "/api", AuctionWeb do
+    #   pipe_through :api
+    # end
+
+    # Enables LiveDashboard only for development
+    #
+    # If you want to use the LiveDashboard in production, you should put
+    # it behind authentication and allow only admins to access it.
+    # If your application does not have an admins-only section yet,
+    # you can use Plug.BasicAuth to set up some basic authentication
+    # as long as you are also using SSL (which you should anyway).
+    if Mix.env() in [:dev, :test] do
+        import Phoenix.LiveDashboard.Router
+
+        scope "/" do
+            pipe_through :browser
+            live_dashboard "/dashboard", metrics: AuctionWeb.Telemetry
+        end
+    end
+
+    # Enables the Swoosh mailbox preview in development.
+    #
+    # Note that preview only shows emails that were sent by the same
+    # node running the Phoenix server.
+    if Mix.env() == :dev do
+        scope "/dev" do
+            pipe_through :browser
+
+            forward "/mailbox", Plug.Swoosh.MailboxPreview
+        end
+    end
 end
